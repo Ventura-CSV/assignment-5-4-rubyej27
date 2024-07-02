fbe8aa2649d5d12a8116d0bf5cbd7b1dc64e5874
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-07-02 19:28:37 -0400

    error commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-"""
-########################################
-Code Your lambda Program here
-isspace = lambda 
-########################################
-"""
-
+isspace = lambda v: True if v = " " or v = "\t" or v = "\n" else False
 
 def mystrip(strval):
-    strip = ''
-    for v in strval:
-        if not isspace(v):
-            strip += v
-    return strip
+   strip = ''
+   for v in strval:
+       if not isspace(v):
+           strip += v
+   return strip
 
 
 strval = 'Python Programming\t Section 1\n'
